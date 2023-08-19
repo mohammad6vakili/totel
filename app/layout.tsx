bb728f494e2eb46ab758d599ab415e1e24d53366
mohammad6vakili <mohammad6vakili@gmail.com>
2023-08-19 14:11:44 +0330

    fix(header styles issue): :bug: fix header style issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+        <div className="page_wrapper">{children}</div>
         <Footer />
       </body>
     </html>
