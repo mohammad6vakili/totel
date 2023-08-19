0f87a4f516a3b587f7a9a4a046a72272bbba8eb9
mohammad6vakili <mohammad6vakili@gmail.com>
2023-08-19 09:55:29 +0330

    feat(add header navs): :sparkles: add header navigations

---
+++ assets/icons/arrow_bottom.tsx
@@ -0,0 +1,20 @@
+const ArrowBottom = ({ style }: any) => {
+  return (
+    <svg
+      style={style}
+      width="11"
+      height="6"
+      viewBox="0 0 13 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.8237 1.3335L7.41292 5.74424C7.08749 6.06968 6.55985 6.06968 6.23441 5.74424L1.82367 1.3335"
+        stroke="black"
+        stroke-width="1.67"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+export default ArrowBottom;
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,6 +1,50 @@
+"use client";
 import styles from "./header.module.css";
+import Logo from "@/assets/images/logo";
+import ArrowBottom from "@/assets/icons/arrow_bottom";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { Popover } from "antd";
 
 const Header = () => {
-  return <div>Header</div>;
+  const pathname = usePathname();
+  const companyPopoverContent = (
+    <ul className={styles.header_companies_dropdown}>
+      <li>
+        <Link href={"/"}>Company 1</Link>
+      </li>
+      <li>
+        <Link href={"/"}>Company 2</Link>
+      </li>
+      <li>
+        <Link href={"/"}>Company 3</Link>
+      </li>
+    </ul>
+  );
+  return (
+    <div className={styles.header}>
+      <Link href={"/"} className={styles.header_logo_section}>
+        <Logo />
+      </Link>
+      <ul className={styles.header_links}>
+        <li className={pathname === "/" ? styles.header_link_active : ""}>
+          <Link href={"/"}>Rooms</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Looking for Roommate</Link>
+        </li>
+        <li>
+          <Popover content={companyPopoverContent} title={null}>
+            <Link href={"/"}>
+              Company <ArrowBottom style={{ marginLeft: 5 }} />
+            </Link>
+          </Popover>
+        </li>
+        <li>
+          <Link href={"/"}>Help</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Header;
