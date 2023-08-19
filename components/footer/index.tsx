f8b47d7e7c98df259329bf6d0ff7314bc8a07edd
mohammad6vakili <mohammad6vakili@gmail.com>
2023-08-19 13:37:49 +0330

    feat(add footer bottom section): :sparkles: add footer bottom section ui

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,6 +1,13 @@
+// next imports
 import Link from "next/link";
 import styles from "./footer.module.css";
+
+// assets imports
 import Logo from "@/assets/images/logo";
+import FacebookIcons from "@/assets/icons/facebook";
+import GoogleIcon from "@/assets/icons/google";
+import AppleIcon from "@/assets/icons/apple";
+import InstagramIcon from "@/assets/icons/instagram";
 
 const Footer = () => {
   return (
@@ -11,6 +18,20 @@ const Footer = () => {
           <Logo />
         </Link>
         <span>© 2023 Totel. All Rights Reserved.</span>
+        <div>
+          <Link href={"https://facebook.com"}>
+            <FacebookIcons />
+          </Link>
+          <Link href={"https://google.com"}>
+            <GoogleIcon />
+          </Link>
+          <Link href={"https://apple.com"}>
+            <AppleIcon />
+          </Link>
+          <Link href={"https://instagram.com"}>
+            <InstagramIcon />
+          </Link>
+        </div>
       </div>
     </footer>
   );
