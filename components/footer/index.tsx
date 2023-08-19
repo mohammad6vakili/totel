33de29adb0de9ee6dd2efaa1660f2369e34e6478
mohammad6vakili <mohammad6vakili@gmail.com>
2023-08-19 13:50:25 +0330

    feat(add footer section top): :sparkles: add footer section top

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -8,11 +8,52 @@ import FacebookIcons from "@/assets/icons/facebook";
 import GoogleIcon from "@/assets/icons/google";
 import AppleIcon from "@/assets/icons/apple";
 import InstagramIcon from "@/assets/icons/instagram";
+import GooglePlay from "@/assets/images/google_play.png";
+import AppStore from "@/assets/images/app_store.png";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>1</div>
+      {/* top section */}
+      <div>
+        <div>
+          <h6>Products</h6>
+          <Link href={"/"}>Features</Link>
+          <Link href={"/"}>Integrations</Link>
+          <Link href={"/"}>Enterprise</Link>
+          <Link href={"/"}>Solutions</Link>
+        </div>
+        <div>
+          <h6>Products</h6>
+          <Link href={"/"}>Features</Link>
+          <Link href={"/"}>Integrations</Link>
+          <Link href={"/"}>Enterprise</Link>
+          <Link href={"/"}>Solutions</Link>
+        </div>
+        <div>
+          <h6>Resources</h6>
+          <Link href={"/"}>Partners</Link>
+          <Link href={"/"}>Developers</Link>
+          <Link href={"/"}>Community</Link>
+          <Link href={"/"}>Apps</Link>
+        </div>
+        <div>
+          <h6>Company</h6>
+          <Link href={"/"}>About Us</Link>
+          <Link href={"/"}>Careers</Link>
+        </div>
+        <div>
+          <h6>Mobile app</h6>
+          <Link href={"https://apps.apple.com"}>
+            <Image src={AppStore} alt="app store" />
+          </Link>
+          <Link href={"https://play.google.com"}>
+            <Image src={GooglePlay} alt="google play" />
+          </Link>
+        </div>
+      </div>
+      {/* bottom section */}
       <div>
         <Link href={"/"}>
           <Logo />
