edb8da418f954e1fe7232f6cdd3449221bf3830b
mohammad6vakili <mohammad6vakili@gmail.com>
2023-08-19 16:40:09 +0330

    feat(add render loop): :sparkles: add rooms render loop

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import "swiper/css/pagination";
 import Filterbar from "@/components/filterbar";
 import styles from "./page.module.css";
 import Searchbar from "@/components/searchbar";
+import { ROOMS } from "@/data/rooms";
+import { Fragment } from "react";
 
 const Home = () => {
   return (
@@ -12,6 +14,14 @@ const Home = () => {
       <Searchbar />
       {/* filterbar */}
       <Filterbar />
+      <div className={styles.rooms_result_container}>
+        <div style={{ display: "none" }}>Map</div>
+        <div className={styles.rooms_result_list}>
+          {ROOMS.map((room, index) => (
+            <Fragment key={index}>{index}</Fragment>
+          ))}
+        </div>
+      </div>
     </main>
   );
 };
